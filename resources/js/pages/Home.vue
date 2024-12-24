c32ed37d6455cc512128c3a88873f31ea765ea65
Lucas Yang <yangchenshin77@gmail.com>
2024-12-24 18:25:14 +0800

    Update home links style

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -2,9 +2,9 @@
   <div>
     <h2 class="text-2xl font-bold">Inertia v2 新功能 Demo</h2>
 
-    <ul class="mt-4 border divide-y rounded-md overflow-hidden">
+    <ul class="mt-4 border-2 border-indigo-500 divide-y-2 divide-indigo-500">
       <li v-for="item in nav" :key="item.href">
-        <Link :href="item.href" class="group block px-4 py-2.5 hover:bg-indigo-600 text-gray-600 hover:text-white transition-colors duration-75">
+        <Link :href="item.href" class="group block px-4 py-2.5 hover:bg-indigo-500 text-gray-600 hover:text-white transition-colors duration-75">
           <h5 class="font-bold">
             {{ item.title }}<template v-if="item.zhTitle"> ({{ item.zhTitle }})</template>
           </h5>
