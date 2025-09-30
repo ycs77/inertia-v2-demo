07406abf70f68e5cf9cb323e950c1cfc9fbf41e4
Lucas Yang <yangchenshin77@gmail.com>
2025-10-01 03:08:07 +0800

    Mark nav items as v2.0 features

--- resources/js/nav.js
+++ resources/js/nav.js
@@ -4,24 +4,28 @@ export const nav = [
     zhTitle: '輪巡',
     href: '/polling',
     description: '使用 usePoll() 輪巡定時拉取新資料',
+    version: '2.0',
   },
   {
     title: 'Prefetching',
     zhTitle: '預先載入',
     href: '/prefetching',
     description: '鼠標滑入連結時，預先載入該連結頁面的資料',
+    version: '2.0',
   },
   {
     title: 'Deferred props',
     zhTitle: '延遲載入 Prop',
     href: '/deferred-props',
     description: '不預先輸入資料到 HTML，而是當 HTML 載入完成之後才載入資料',
+    version: '2.0',
   },
   {
     title: 'Lazy loading',
     zhTitle: '惰性載入',
     href: '/lazy-loading',
     description: '當元素出現在畫面中時，才載入資料',
+    version: '2.0',
   },
   // // @TODO: 現在無法使用，預計 Inertia 2.1 會支援
   // {
@@ -29,5 +33,6 @@ export const nav = [
   //   zhTitle: '無限滾動',
   //   href: '/infinite-scroll',
   //   description: '無限滾動載入資料',
+  //   version: '2.2',
   // },
 ]
--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -3,7 +3,7 @@
     <h2 class="text-2xl font-bold">Inertia v2 新功能 Demo</h2>
 
     <ul class="mt-4 border-2 border-indigo-500 divide-y-2 divide-indigo-500">
-      <li v-for="item in nav" :key="item.href">
+      <li v-for="item in nav_20" :key="item.href">
         <Link :href="item.href" class="group block px-4 py-2.5 hover:bg-indigo-500 text-gray-600 hover:text-white transition-colors duration-75">
           <h5 class="font-bold">
             {{ item.title }}<template v-if="item.zhTitle"> ({{ item.zhTitle }})</template>
@@ -19,4 +19,6 @@
 
 <script setup>
 import { nav } from '../nav'
+
+const nav_20 = nav.filter(item => item.version === '2.0')
 </script>
